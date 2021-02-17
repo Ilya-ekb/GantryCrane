663b5f895de2fad981e4befc2a2175db47fe8782
Apparat Normalniy <2190055@gmail.com>
2021-02-17 09:46:53 +0500

    Реализовано перемещение вперед-назад

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public MotorController moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float signal;
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moving.Working(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moving.Working(1);
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            moving.Working(0);
+        }
+    }
+}
